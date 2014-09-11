ee605a7ee73940082c4b1625a79d5fb319cff737
Ektor5 <ektor-5@udoo.org>
2014-09-11 15:14:51 +0000

    Added in output files

--- dev/testing/pyo_sine_wave.py
+++ dev/testing/pyo_sine_wave.py
@@ -3,13 +3,14 @@ import time
 
 s = Server(audio="jack").boot()
 s.start()
-mod = Sine(freq=32, mul=50)
-a = Sine(freq=mod, mul=0.01).mix(3).out()
+s.recstart()
+a = Sine(mul=0.01).mix(2).out()
 
 # keep thread alive
 try:
     while True:
         time.sleep(1)
 except KeyboardInterrupt:
+    s.recstop()
     s.stop()
     
