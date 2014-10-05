205295f514baa1e9e5d615453cc030c46b350b01
Yao Sun <mr.sun.yao@gmail.com>
2014-10-04 22:28:06 -0400

    create seperate mac directory for testing

---
+++ dev/testing-mac/pyo_sine_pan_phase.py
@@ -0,0 +1,19 @@
+from pyo import *
+
+s = Server(audio="jack", nchnls=2).boot()
+s.start()
+s.recstart()
+
+a = Sine(freq=261.6, mul=0.05)
+b = Sine(freq=261.6, phase=0.5, mul=0.05)
+p = Pan(a, outs=2, pan=1, spread=0).out()
+p2 = Pan(b, outs=2, pan=0, spread=0).out()
+
+
+
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    s.stop()
+    s.recstop()
