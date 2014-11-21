3ae8c9af4cd844abfa902a3cf7646395ed5852ab
Yao Sun <mr.sun.yao@gmail.com>
2014-11-20 21:47:07 -0500

    test

--- dev/MATLAB/input_dft/dtft_input_plot.m
+++ dev/MATLAB/input_dft/dtft_input_plot.m
@@ -2,11 +2,12 @@ function [ output_args ] = dtft_input_plot(input_arg)
 %DTFT_INPUT_PLOT Summary of this function goes here
 %   Detailed explanation goes here
 
-[xn fs]=wavread('input1.wav');
+[xn fs]=wavread(input_arg);
 nf=106000; %number of point in DTFT
 Y1 = fft(xn,nf);
+Y1db = mag2db(Y1);
 f1 = fs/2*linspace(0,1,nf/2+1);
-plot(f1,abs(Y1(1:nf/2+1)));
+plot(f1,abs(Y1db(1:nf/2+1)));
 
 end
 
