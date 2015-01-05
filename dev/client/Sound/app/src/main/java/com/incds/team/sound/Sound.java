dd5745b4d9f62a6dcd8c53866b5335f640772ac6
Yao Sun <mr.sun.yao@gmail.com>
2015-01-04 21:06:17 -0500

    commit

--- dev/client/Sound/app/src/main/java/com/incds/team/sound/Sound.java
+++ dev/client/Sound/app/src/main/java/com/incds/team/sound/Sound.java
@@ -11,6 +11,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
