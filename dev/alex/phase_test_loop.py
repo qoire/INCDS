fe51883c91a58d2c992e3723991b3834978a57bb
alexanderleung09 <alexanderleung09@gmail.com>
2015-02-06 02:37:30 -0500

    Done

--- dev/alex/phase_test_loop.py
+++ dev/alex/phase_test_loop.py
@@ -10,58 +10,58 @@ class phaseModule():
     # Alex keep your code here
     def __init__(self):
         # initiate self variables here
-        self.phase_list = []
-        self.current_phase = 0
+		self.phase_list = []
+		self.current_phase = 0
+		self.counter = 0
+		self.increment=30*3.1415/180
+		self.prev_phase=None
+		self.prev_mag=None
+		self.is_phase_going_negative=None
+		
 
     def setPhase(self, phase):
         self.current_phase = phase
 
     def phaseChange(self, cur_mag):
-
+		if self.counter==0:
+			next_phase=5*3.1415/180
+			self.ini_mag=cur_mag
+			self.counter=self.counter+1
+		elif self.counter==1:
+			if cur_mag > self.ini_mag:
+				next_phase=-10*3.1415/180
+				self.prev_phase=0
+				self.prev_mag=cur_mag
+				self.is_phase_going_negative=True
+				self.counter=self.counter+1
+			else:
+				next_phase=10*3.1415/180
+				self.prev_phase=0
+				self.prev_mag=cur_mag
+				self.is_phase_going_negative=False
+				self.counter=self.counter+1
+		else:
+			if cur_mag <= self.prev_mag:
+				if self.is_phase_going_negative==True:
+					next_phase=self.prev_phase-self.increment
+					self.prev_mag=cur_mag
+				else:
+					next_phase=self.prev_phase+self.increment
+					self.prev_mag=cur_mag
+				self.prev_phase=next_phase
+			else:
+				self.increment=self.increment/2
+				self.prev_mag=cur_mag
+				if self.is_phase_going_negative==True:
+					next_phase=self.prev_phase+self.increment
+				else:
+					next_phase=self.prev_phase-self.increment
+				self.is_phase_going_negative=not(self.is_phase_going_negative)
+				self.prev_phase=next_phase
+				
         #main function that will contain the phase change algorithm
-
-        min=sinfunc(0)#call taylor's function
-        cursor=sinfunc(15)
-        increment=30
-
-        if cursor>min:
-            buff=-10
-            cursor=sinfunc(buff)
-            isdecre=true
-
-            while increment > 0.1:
-                while cursor<=min:
-                    min=cursor
-                    if isdecre==1:
-                        buff=buff-increment
-                    else:
-                        buff=buff+increment
-
-                    cursor=sinfunc(buff)#call taylor's function
-
-            min=cursor
-            increment=increment/2
-            isdecre=not(isdecre)
-        else:
-            buff=10
-            cursor=sinfunc(buff)
-            isdecre=false
-            while increment > 0.1:
-                while cursor<=min:
-                    min=cursor
-                    if isdecre==1:
-                        buff=buff-increment
-                    else:
-                        buff=buff+increment
-                    
-                    cursor=sinfunc(buff)#call taylor's function
-
-                min=cursor
-                increment=increment/2
-                isdecre=not(isdecre)
-
-        next_phase = 0
-        return next_phase
+		print " self.increment=%s " %self.increment
+		return next_phase
 
 # MAIN START
 MIN_RAND = 1
@@ -81,10 +81,14 @@ min_val = var_c - var_a
 #instantiate class
 ph = phaseModule()
 ph.setPhase(0)
-
+i=0
 nx_phase = 0 #initially set to 0
 while True:
-    mag = var_a*(math.cos(var_b*(-var_d+nx_phase))) + var_c 
-    nx_phase = ph.phaseChange(mag)
-    print "Magnitude:" + str(mag) + " Next Phase:" + str(nx_phase) + "Target mag:" + str(min_val)
-    time.sleep(0.2)
+	mag = var_a*(math.cos(var_b*(-var_d+nx_phase))) + var_c 
+	nx_phase = ph.phaseChange(mag)
+	print "Magnitude:" + str(mag) + " Next Phase:" + str(nx_phase) + " Target mag:" + str(min_val)
+	time.sleep(0.2)
+	if i>40:
+		break
+	else:
+		i=i+1
