8fa27f218c4b2b0723b34c1ce69636c4355d8f43
Yao Sun <mr.sun.yao@gmail.com>
2015-07-28 01:15:21 -0400

    Refactored clutter

--- dev/taylor-input/channel_test.py
+++
@@ -1,8 +0,0 @@
-from pyo import *
-
-#print pa_list_devices()
-s = Server()
-s.setInputDevice(3)
-s.setOutputDevice(3)
-s.boot()
-
--- dev/taylor-input/device_test.py
+++
@@ -1,9 +0,0 @@
-from pyo import *
-pa_list_devices()
-max_in = pa_get_input_max_channels(1)
-max_out = pa_get_output_max_channels(2)
-print max_in
-print max_out
-
-print "pa_get_default_input:" + str(pa_get_default_input())
-print "pa_get_default_output:" + str(pa_get_default_output())
